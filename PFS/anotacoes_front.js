eeab9b5c0e4743bbab650b3543c2f85ca299dc72
Mars2035eng <81594800+Mars2035eng@users.noreply.github.com>
2022-11-09 21:44:04 -0300

    Create anotacoes_front.js

---
+++ PFS/anotacoes_front.js
@@ -0,0 +1,15 @@
+1) instalar o pacote:
+   npm install -g json-server
+
+2) Create a db.json file with some data
+
+3) Start JSON Server:
+   npm json-server --watch db.json
+   ou
+   npx json-server --watch db.json
+
+
+
+
+
+
